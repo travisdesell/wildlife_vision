469023981e339e3fa52c73c88811b84642946580
Marshall Mattingly <marshall.p.mattingly@und.edu>
2016-11-04 14:19:31 -0500

    updated mosaic splitter to be by size

--- jxl_parser/image_splitter/mosaic_splitter.py
+++ jxl_parser/image_splitter/mosaic_splitter.py
@@ -32,29 +32,67 @@ class SplitImage:
         self.filename = os.path.join(directory, name)
         self.number = number
         self.imageId = 0
+        self.size = 0
 
-def split_and_save_images(image, directory, split):
-    print "Slicing image: {} into {} pieces".format(image.filename, split)
+def split_and_save_images(image, directory, max_tile_width, max_tile_height):
+    print "Slicing image: {} into with max dimensions {}x{}".format(image.filename, max_tile_width, max_tile_height)
 
     # split the image without saving
     try:
         tiles = []
 
-        with Image(filename=image.filename) as img:
-            d = int(math.sqrt(split))
-            w = int(img.size[0] / d)
-            h = int(img.size[1] / d)
+        with Image(filename=image.filename) as base_img:
+            if base_img.sequence:
+                img = base_img.sequence[0]
+            else:
+                img = base_img
 
-            for y in range(d):
-                for x in range(d):
-                    x_loc = x * w
-                    y_loc = y * h
-                    number =  y * d + x
+            print img.size
 
+            # get the number of columns and the width of each tile
+            cols = int(math.ceil(img.size[0] / float(max_tile_width)))
+            tile_width = max_tile_width
+
+            # the last two tiles are special
+            if cols > 1:
+                last_tiles_width = img.size[0] - (tile_width * (cols - 2))
+                last_tiles_width = int(last_tiles_width / 2)
+            else:
+                last_tiles_width = img.size[0]
+
+            # get the number of rows and the height of each tile
+            rows = int(math.ceil(img.size[1] / float(max_tile_height)))
+            tile_height = max_tile_height
+
+            # the final two rows is special
+            if rows > 1:
+                last_tiles_height = img.size[1] - (tile_height * (rows - 2))
+                last_tiles_height = int(last_tiles_height / 2)
+            else:
+                last_tiles_height = img.size[1]
+
+            print img.size[0], 'x', img.size[1], ' => ', rows, 'x', cols
+
+            y_loc = 0
+            number = 0
+            for y in range(rows):
+                if y >= (rows - 2):
+                    height = last_tiles_height
+                else:
+                    height = tile_height
+
+                x_loc = 0
+                for x in range(cols):
+                    if x >= (cols - 2):
+                        width = last_tiles_width
+                    else:
+                        width = tile_width
+
+                    number = number + 1
                     tile = SplitImage(
-                        name='{}.png'.format(number+1),
+                        name='{}.png'.format(number),
                         x=x_loc, y=y_loc,
-                        width=w, height=h,
+                        width=width, height=height,
                         directory=directory,
                         number=number
                     )
@@ -68,12 +106,23 @@ def split_and_save_images(image, directory, split):
                     print '\t', tile.filename
 
                     with img.clone() as i:
-                        i.crop(x_loc, y_loc, width=w, height=h)
+                        print '\t\t(', x_loc, ',', y_loc, ') =>', width, 'x', height
+                        i.crop(x_loc, y_loc, width=width, height=height)
                         i.format = 'png'
                         i.save(filename=tile.filename)
+                        tile.size = os.path.getsize(tile.filename)
                         tiles.append(tile)
 
-        if not tiles or len(tiles) < split:
+                    x_loc = x_loc + width
+
+                y_loc = y_loc + height
+
+            # cleanup our sequence files
+            if base_img.sequence:
+                for x in range(len(base_img.sequence)):
+                    base_img.sequence[x].destroy()
+
+        if not tiles or len(tiles) < (rows * cols):
             print "\tError slicing image"
             return None
     except:
@@ -84,7 +133,7 @@ def split_and_save_images(image, directory, split):
     # return the split images array
     return tiles
 
-def split_image_to_db(image, cnx, directory, split):
+def split_image_to_db(image, cnx, directory, max_tile_width, max_tile_height):
     """Splits an image into multiple images and saves the to the given
     directory and stores their metadata in the database.
     """
@@ -93,8 +142,8 @@ def split_image_to_db(image, cnx, directory, split):
 
     addImage = (
         "INSERT INTO images "
-        "(`datetime`, `archive_filename`, `camera_id`, `species`, `year`, `project_id`) "
-        "VALUES ('{}', '{}', '0', 2, 2016, '{}')"
+        "(`datetime`, `archive_filename`, `camera_id`, `species`, `year`, `project_id`, `size`) "
+        "VALUES ('{}', '{}', '0', 2, 2016, '{}', '{}')"
     )
 
     addSplitImage = (
@@ -119,7 +168,7 @@ def split_image_to_db(image, cnx, directory, split):
 
         # see if we can get the splits from the images table but not the mosaic_split_images table
         # split and save the files
-        tiles = split_and_save_images(image, directory, split)
+        tiles = split_and_save_images(image, directory, max_tile_width, max_tile_height)
         if not tiles:
             print "No tiles created for", image.imageId
             return False
@@ -134,7 +183,8 @@ def split_image_to_db(image, cnx, directory, split):
             statement = addImage.format(
                 now.strftime("%Y-%m-%d %H:%M:%S"),
                 tile.filename,
-                image.project_id
+                image.project_id,
+                tile.size
             )
 
             count = count + 1
@@ -146,6 +196,7 @@ def split_image_to_db(image, cnx, directory, split):
         print 'Added {} split images to the main table.\n'.format(count)
         count = 0
 
+        empty = 0
         for tile in tiles:
             # crappy way to do this, but it's the way i'm doing it
             statement = addSplitImage.format(
@@ -159,6 +210,9 @@ def split_image_to_db(image, cnx, directory, split):
             )
             print "\t", statement
 
+            if tile.size < 100000:
+                empty = empty + 1
+
             cursor.execute(statement)
             count = count + 1
             print '\tAdded:', tile.filename
@@ -166,6 +220,13 @@ def split_image_to_db(image, cnx, directory, split):
         # save the changes
         cnx.commit()
         print 'Added {} split images to mosaic table.\n'.format(count)
+
+        # update the mosaic count
+        statement = 'UPDATE mosaic_images SET split_count = {}, empty_count = {} WHERE id = {}'.format(len(tiles), empty, image.imageId)
+        cursor.execute(statement)
+        print 'Updatd the mosaic_images.split_count to {} and empty_count to {}'.format(len(tiles), empty)
+        cnx.commit()
+
         added = added + 1
     except:
         print 'Error submitting split images to the database.'
@@ -177,7 +238,7 @@ def split_image_to_db(image, cnx, directory, split):
 
     return success
 
-def split_all_images_to_db(host, database, username, password, directory, split):
+def split_all_images_to_db(host, database, username, password, directory, max_tile_width, max_tile_height):
     """Splits all the images (that haven't been split) and stores
     the saved split images to the database and directory."""
 
@@ -235,7 +296,8 @@ def split_all_images_to_db(host, database, username, password, directory, split)
                 image=image,
                 cnx=cnx2,
                 directory=outdir,
-                split=split
+                max_tile_width=max_tile_width,
+                max_tile_height=max_tile_height
             )
 
             # next row
@@ -271,7 +333,8 @@ if __name__ == '__main__':
     argparser.add_argument('--password', type=str,
         help='Password for database connection', default=''
     )
-    argparser.add_argument('--split', type=int, help='Amount to split images into (default: 100)', default=100)
+    argparser.add_argument('--max_tile_width', type=int, help='Maximum width of each tile (default: 1024)', default=1024)
+    argparser.add_argument('--max_tile_height', type=int, help='Maximum height of each tile (default: 768)', default=768)
     args = argparser.parse_args()
 
     split_all_images_to_db(
@@ -280,5 +343,6 @@ if __name__ == '__main__':
         username=args.username,
         password=args.password,
         directory=args.directory,
-        split=args.split
+        max_tile_width=args.max_tile_width,
+        max_tile_height=args.max_tile_height
     )
